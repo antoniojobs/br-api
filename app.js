5bf54055d76d56ecff4664a4dfc69f8e0657b69e
José Antonio <58563869+antoniojobs@users.noreply.github.com>
2023-06-16 14:43:23 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+const estados = [
+    { nome: 'Acre', sigla: 'AC' },
+    { nome: 'Alagoas', sigla: 'AL' },
+    { nome: 'Amapá', sigla: 'AP' },
+    { nome: 'Amazonas', sigla: 'AM' },
+    { nome: 'Bahia', sigla: 'BA' },
+    { nome: 'Ceará', sigla: 'CE' },
+    { nome: 'Distrito Federal', sigla: 'DF' },
+    { nome: 'Espírito Santo', sigla: 'ES' },
+    { nome: 'Goiás', sigla: 'GO' },
+    { nome: 'Maranhão', sigla: 'MA' },
+    { nome: 'Mato Grosso', sigla: 'MT' },
+    { nome: 'Mato Grosso do Sul', sigla: 'MS' },
+    { nome: 'Minas Gerais', sigla: 'MG' },
+    { nome: 'Pará', sigla: 'PA' },
+    { nome: 'Paraíba', sigla: 'PB' },
+    { nome: 'Paraná', sigla: 'PR' },
+    { nome: 'Pernambuco', sigla: 'PE' },
+    { nome: 'Piauí', sigla: 'PI' },
+    { nome: 'Rio de Janeiro', sigla: 'RJ' },
+    { nome: 'Rio Grande do Norte', sigla: 'RN' },
+    { nome: 'Rio Grande do Sul', sigla: 'RS' },
+    { nome: 'Rondônia', sigla: 'RO' },
+    { nome: 'Roraima', sigla: 'RR' },
+    { nome: 'Santa Catarina', sigla: 'SC' },
+    { nome: 'São Paulo', sigla: 'SP' },
+    { nome: 'Sergipe', sigla: 'SE' },
+    { nome: 'Tocantins', sigla: 'TO' }
+];
+
+app.get('/estados', (req, res) => {
+    res.json(estados);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
